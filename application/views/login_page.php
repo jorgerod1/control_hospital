981a60ea2551a28135177ac686692e8f5d5a12b0
unknown <jorgerodriguez31098@gmail.com>
2023-02-22 22:03:38 -0600

    2

--- application/views/login_page.php
+++ application/views/login_page.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	
-    <title>Iniciar sesiónn</title>
+    <title>Iniciar sesiónny</title>
 
     <style type="text/css">
 
